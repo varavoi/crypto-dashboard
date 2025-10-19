4c6b868fd6f317ee12fe1ed84feb4a6fcbcaa474
varavoi <institut93@gmail.com>
2025-10-19 16:49:11 +0300

    feat: create initial project structure and basic App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styles from './App.module.css'
 
 function App() {
+  const [message] = useState('Crypto Dashboard is initializing...')
   return (
-    <div className="App">
-
+     <div className={styles.app}>
+      {/* Заголовок первого уровня */}
+      <h1>Crypto Dashboard</h1>
+      {/* Отображаем значение из состояния message */}
+      <p>{message}</p>
     </div>
   );
 }
